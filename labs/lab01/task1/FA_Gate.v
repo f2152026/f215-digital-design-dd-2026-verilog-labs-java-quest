e703f6a0b7d7d398cae9932e57d03ba2520c0cca
NISCHAY KUMAR <nischayk94@gmail.com>
2026-08-26 21:19:48 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -16,10 +16,11 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  xor (ps,  a,   b);
-  and (pc1, a,   b);
-  xor (sum, cin, ps);
-  and (pc2, cin, ps);
-  or  (cout, pc1, pc2);
+  or (cout, pc2, pc1);
+  and (pc2, ps, cin);
+  and (pc1, a, b);
+  xor (sum, ps, cin);
+  xor (ps, a, b);
+
 
 endmodule
